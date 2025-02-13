59be3c967bdb0025c1d4db145d7f0329652d1a1a
Théo <dill.fisselbrand.theo@gmail.com>
2025-02-14 00:21:53 +0100

    Test

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
+import MusicListApp from "./MusicListApp";
+import "./App.css";
 
-function App() {
-    const [count, setCount] = useState(0)
-
+function App()
+{
+    const isActive: boolean = true;
     return (
-        <>
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
+        <Router>
+            <div className={"App"}>
+                <header className={"app-header"}>
+                    <nav className={"app-nav"}>
+                        <NavLink
+                            to={"/test"}
+                            end
+                            className={({ isActive }: { isActive: boolean }) =>
+                                isActive ? "nav-link active" : "nav-link"
+                            }
+                        >
+                            Répertoire
+                        </NavLink>
+                    </nav>
+                </header>
+
+                <main className={"app-main"}>
+                    <Routes>
+                        <Route path={"/test"} element={<MusicListApp />} />
+                    </Routes>
+                </main>
             </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
-    )
+        </Router>
+    );
 }
 
-export default App
-
-
-
-// import React from "react";
-// import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
-// import MusicListApp from "./MusicListApp";
-// import "./App.css";
-//
-// function App()
-// {
-//     const isActive: boolean = true;
-//     return (
-//         <Router>
-//             <div className={"App"}>
-//                 <header className={"app-header"}>
-//                     <nav className={"app-nav"}>
-//                         <NavLink
-//                             to={"/"}
-//                             end
-//                             className={({ isActive }: { isActive: boolean }) =>
-//                                 isActive ? "nav-link active" : "nav-link"
-//                             }
-//                         >
-//                             Répertoire
-//                         </NavLink>
-//                     </nav>
-//                 </header>
-//
-//                 <main className={"app-main"}>
-//                     <Routes>
-//                         <Route path={"/Mystique"} element={<MusicListApp />} />
-//                     </Routes>
-//                 </main>
-//             </div>
-//         </Router>
-//     );
-// }
-//
-// export default App;
+export default App;
