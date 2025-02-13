a32080e016be017cde8fd02d75ef8383b43c3adc
Théo <dill.fisselbrand.theo@gmail.com>
2025-02-13 23:55:27 +0100

    Correction

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Mystique",
-  root: "./",
+  base: "/Mystique/",
   build: {
     outDir: "dist",
-  },
+    assetsDir: "assets",
+    emptyOutDir: true,
+  }
 });
