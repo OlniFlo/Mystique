8666e630f4165adc6762c8f3a82b9884c8806740
Théo <dill.fisselbrand.theo@gmail.com>
2025-02-15 15:13:12 +0100

    Tentative de correction de l'actualisation de la page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useRef, useState} from "react";
-import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, NavLink } from "react-router-dom";
 import MusicListApp from "./MusicListApp";
 import "./App.css";
 import MusicSetlistApp from "./MusicSetlistApp";
